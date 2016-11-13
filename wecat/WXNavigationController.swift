bd54e711a24ff712c8e18dde6c7f46e14c816db8
Sandm <ashley@AshleydeMacBook-Pro-4.local>
2016-11-13 23:01:46 +0800

    修正状态栏颜色

--- wecat/WXNavigationController.swift
+++ wecat/WXNavigationController.swift
@@ -12,7 +12,7 @@ class WXNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationBar.barTintColor = UIColor.black //状态栏颜色
+        self.navigationBar.barTintColor = #colorLiteral(red: 0.2274509804, green: 0.2235294118, blue: 0.2470588235, alpha: 1) //状态栏颜色
         self.navigationBar.tintColor = UIColor.white    //左右按钮颜色
         self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]    //修改标题颜色
     }
