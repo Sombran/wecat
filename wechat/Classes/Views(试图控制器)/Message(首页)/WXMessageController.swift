551b7d0edea961cf6caffe60686923452ee4b498
Sandm <ashley@AshleydeMacBook-Pro-4.local>
2016-11-15 21:41:35 +0800

    添加表格，并为首页绑定假数据

--- wechat/Classes/Views(试图控制器)/Message(首页)/WXMessageController.swift
+++ wechat/Classes/Views(试图控制器)/Message(首页)/WXMessageController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
+private let cellId = "cellId"
 class WXMessageController: WXBaseViewController {
 
+    lazy var dataList = [String]()
+    
     @objc func add(){
         print("添加")
     }
+    override func loadData() {
+        for i in 0..<10 {
+            dataList.insert("\(i)", at: 0)
+        }
+    }
 }
+//实现数据源
+extension WXMessageController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = dataList[indexPath.row]
+        return cell
+    }
+}
+
+//设置UI
 extension WXMessageController{
     override func setUI() {
         super.setUI()
         let img = UIImage(named: "barbuttonicon_add")?.withRenderingMode(.alwaysOriginal)
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: img, style: .plain, target: self, action: #selector(add))
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 }
--- wechat/Classes/Views(试图控制器)/Mian/WXBaseViewController.swift
+++ wechat/Classes/Views(试图控制器)/Mian/WXBaseViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class WXBaseViewController: UIViewController {
+    //表格数据由具体子类实现
+    var tableView: UITableView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadData()
         setUI()
-
+        tableView?.dataSource = self
+        tableView?.delegate = self
+    }
+    //加载数据，由子类实现
+    func loadData(){
+        
     }
 
 }
 extension WXBaseViewController{
     func setUI(){
-        
+        setupTableView()
+    }
+    func setupTableView(){
+        tableView = UITableView(frame: view.bounds)
+        view.addSubview(tableView!)
+    }
+}
+//实现数据源方法，由子类实现，不需要super
+extension WXBaseViewController: UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50;
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
